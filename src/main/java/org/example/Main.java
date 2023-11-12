bf0e9a8e6a77f3bd85a0e72306af55e7383b6180
Yuliya <yuliya.zotova@me.com>
2023-11-12 18:33:09 +0200

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello humans! We came with peace");
+        System.out.println("Hello humans!");
     }
 }
